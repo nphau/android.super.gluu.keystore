ef7bc523202d5309e19bebb11acf099194018f5a
NazarYavornytskyy <nazaryavornytskyy@gmail.com>
2016-03-21 16:07:55 +0200

    Committed all files from Pods

---
+++ IOS/Pods/Target Support Files/PAPasscode/PAPasscode-umbrella.h
@@ -0,0 +1,7 @@
+#import <UIKit/UIKit.h>
+
+#import "PAPasscodeViewController.h"
+
+FOUNDATION_EXPORT double PAPasscodeVersionNumber;
+FOUNDATION_EXPORT const unsigned char PAPasscodeVersionString[];
+
