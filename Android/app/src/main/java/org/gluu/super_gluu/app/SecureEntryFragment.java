192a2d9544e157b06a4511a1fb3d29e98c468c7f
Sam Harris <SamIAm@Sams-MacBook-Pro-2.local>
2018-03-21 15:31:29 -0500

    Initial setup of secure entry screen.

---
+++ Android/app/src/main/java/org/gluu/super_gluu/app/SecureEntryFragment.java
@@ -0,0 +1,85 @@
+package org.gluu.super_gluu.app;
+
+/**
+ * Created by SamIAm on 3/21/18.
+ */
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.RelativeLayout;
+
+import org.gluu.super_gluu.app.base.ToolbarFragment;
+import org.gluu.super_gluu.app.fingerprint.Fingerprint;
+
+import SuperGluu.app.R;
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+/**
+ * Created by SamIAm on 3/21/18.
+ */
+
+public class SecureEntryFragment extends ToolbarFragment {
+
+    @BindView(R.id.nav_drawer_toolbar)
+    Toolbar toolbar;
+
+    @BindView(R.id.fingerprint_relative_layout)
+    RelativeLayout fingerprintRelativeLayout;
+
+    @BindView(R.id.pin_code_relative_layout)
+    RelativeLayout pinCodeRelativeLayout;
+
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+
+        View rootView = inflater.inflate(R.layout.fragment_secure_entry, container, false);
+
+        ButterKnife.bind(this, rootView);
+
+        setDefaultToolbar(toolbar, getString(R.string.add_secure_entry), false);
+
+        setHasOptionsMenu(true);
+
+        fingerprintRelativeLayout.setOnClickListener(view -> {
+
+        });
+
+        pinCodeRelativeLayout.setOnClickListener(view -> {
+
+        });
+
+        Fingerprint fingerprint = new Fingerprint(getContext());
+
+        if(!fingerprint.checkIfFingerprintEnabled()) {
+            fingerprintRelativeLayout.setVisibility(View.GONE);
+        }
+
+        return rootView;
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.menu_secure_entry, menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()) {
+            case R.id.skip_action:
+                return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
+
